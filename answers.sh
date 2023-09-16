5658b4f6a0a8d6e974dd45a9049a8d01d99b0138
Hasan Deeb <h.deeb712@gmail.com>
2023-09-16 18:22:50 +0300

    Joins

--- answers.sh
+++ answers.sh
@@ -1,2 +1,38 @@
-SELECT * FROM students
-SELECT * FROM students WHERE Age >= 30
+
+
+SELECT employees.name, employees.Company, companies.Date 
+FROM companies
+INNER JOIN employees ON companies.name = employees.Company
+
+
+SELECT employees.name, employees.Company, companies.Date 
+FROM companies
+INNER JOIN employees ON companies.name = employees.Company
+WHERE companies.Date < 2000
+
+
+SELECT  employees.name, employees.Company, employees.Role, companies.Date 
+FROM companies
+INNER JOIN employees ON companies.name = employees.Company
+WHERE employees.role = 'Graphic Designer'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
